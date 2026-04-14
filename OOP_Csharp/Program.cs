8c479b1cf354e0a149fb4ed6ef3e9c883cc892be
opilane <opilane@HP-CZC2349HTN>
2026-04-14 09:57:32 +0300

    Add project files.

---
+++ OOP_Csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+namespace OOP_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isik inimene1 = new Isik();
+            //inimene1.Nimi = "Mati";
+            //inimene1.Sünniaasta = 2000;
+            //inimene1.Tervita(); // Väljund: Tere! Mina olen Mati...
+
+
+            Õpetaja õpetaja1 = new Õpetaja();
+            õpetaja1.Nimi = "Marina";
+            õpetaja1.Sünniaasta = 1995;
+            õpetaja1.Aine = "programmeerimine";
+            õpetaja1.Kirjelda();
+            õpetaja1.Õpeta();
+
+            Õpilane õpilane1 = new Õpilane();
+            õpilane1.Nimi = "Yaroslav";
+            õpilane1.Sünniaasta = 2008;
+            õpilane1.Kool = "TTHK";
+            õpilane1.Klass = 1;
+            õpilane1.Kirjelda();
+            õpilane1.Õpi();
+        }
+    }    
+}
---
+++ OOP_Csharp/Õpetaja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Csharp
+{
+    public class Õpetaja : Isik
+    {
+        public string Aine { get; set; }
+
+        public override void Kirjelda()
+        {
+            Console.WriteLine($"Mina olen õpetaja {Nimi} ja ma õpetan: {Aine}.");
+        }
+        public void Õpeta()
+        { 
+            Console.WriteLine($"{Nimi} õpetab ainet: {Aine}.");
+        }
+
+
+    }
+}
